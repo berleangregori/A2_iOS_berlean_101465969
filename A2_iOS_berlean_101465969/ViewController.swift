90d4490258c5c2b09401b44568352618774fb175
Berlean Gregori <berlean.gregori@hotmail.com>
2025-03-22 00:40:17 -0400

    Initial Commit

---
+++ A2_iOS_berlean_101465969/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_berlean_101465969
+//
+//  Created by Joshua Nuezca on 2025-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A2_iOS_berlean_101465969Tests/A2_iOS_berlean_101465969Tests.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS_berlean_101465969Tests.swift
+//  A2_iOS_berlean_101465969Tests
+//
+//  Created by Joshua Nuezca on 2025-03-22.
+//
+
+import Testing
+@testable import A2_iOS_berlean_101465969
+
+struct A2_iOS_berlean_101465969Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
