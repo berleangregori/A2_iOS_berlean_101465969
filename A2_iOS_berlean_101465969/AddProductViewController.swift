7b6ccfe541c0a2782764b78cbc7aef5bb4c82509
Berlean Gregori <berlean.gregori@hotmail.com>
2025-03-22 02:06:18 -0400

    connect UI  elements to new screen

--- A2_iOS_berlean_101465969/AddProductViewController.swift
+++ A2_iOS_berlean_101465969/AddProductViewController.swift
@@ -10,7 +10,23 @@ import UIKit
 class AddProductViewController : UIViewController{
     
     
+    @IBOutlet weak var idTextField: UITextField!
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var descTextField: UITextField!
+    
+    @IBOutlet weak var priceTextField: UITextField!
+    
+    @IBOutlet weak var providerTextField: UITextField!
+    
+    
     override func viewDidLoad(){
         super.viewDidLoad()
     }
+    
+    
+    @IBAction func saveTapped(_ sender: UIButton) {
+    }
+    
 }
