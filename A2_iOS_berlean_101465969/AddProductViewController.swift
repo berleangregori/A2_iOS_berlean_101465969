769023cb87bec998da1ae84bd8acb878b6259282
Berlean Gregori <berlean.gregori@hotmail.com>
2025-03-22 02:02:25 -0400

    created add product view controller file

---
+++ A2_iOS_berlean_101465969/AddProductViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AddProductViewController.swift
+//  A2_iOS_berlean_101465969
+//
+//  Created by Joshua Nuezca on 2025-03-22.
+//
+
+import UIKit
+
+class AddProductViewController : UIViewController{
+    
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
