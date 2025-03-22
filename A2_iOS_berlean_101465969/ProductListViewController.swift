1239d8c42cfacc4aae3c6ed8e9ab59fbb658f23e
Berlean Gregori <berlean.gregori@hotmail.com>
2025-03-22 02:23:01 -0400

    created productlis view  controller

---
+++ A2_iOS_berlean_101465969/ProductListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProductListViewController.swift
+//  A2_iOS_berlean_101465969
+//
+//  Created by Joshua Nuezca on 2025-03-22.
+//
+
+import  UIKit
+
+class ProductListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    
+    override func viewDidLoad(){
+        
+    }
+    
+    
+    
+    
+}
